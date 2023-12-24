50ae9d098b5eede4a79dfe509897b92879c04eaf
Thiago Rosa <thiagosrosa@gmail.com>
2023-12-24 19:41:49 -0300

    feat: create CardCategory component

--- src/components/CardCategory.vue
+++ src/components/CardCategory.vue
@@ -12,4 +12,58 @@ export default {
 }
 </script>
 
-<template>{{ category.name }} {{ category.image }}</template>
+<template>
+  <article class="category">
+    <header class="header__category">
+      <img
+        v-bind:src="`/images/icons/ingredients_category/${category.image}`"
+        alt=""
+        class="image__category"
+      />
+      <h2 class="paragraph-lg name__category">{{ category.name }}</h2>
+    </header>
+    <ul class="ingredients__category">
+      <li v-for="(ingredient, index) in category.ingredients" :key="index">{{ ingredient }}</li>
+    </ul>
+  </article>
+</template>
+
+<style>
+.category {
+  width: 19.5rem;
+  padding: 1rem;
+  border-radius: 1rem;
+  background: var(--branco, #fff);
+  box-shadow: 4px 4px 10px 0px rgba(68, 68, 68, 0.05);
+  height: 100%;
+
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: 2rem;
+}
+
+.header__category {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: 0.5rem;
+}
+
+.image__category {
+  width: 3.5rem;
+}
+
+.name__category {
+  text-align: center;
+  color: var(--verde-medio, #3d6d4a);
+  font-weight: 700;
+}
+
+.ingredients__category {
+  display: flex;
+  justify-content: center;
+  gap: 0.5rem;
+  flex-wrap: wrap;
+}
+</style>
