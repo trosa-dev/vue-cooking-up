e0cbb8a6583ee809af4b485a403bc4ef80e7569d
Thiago Rosa <thiagosrosa@gmail.com>
2023-12-24 18:50:57 -0300

    feat: create CardCategory component

---
+++ src/components/CardCategory.vue
@@ -0,0 +1,15 @@
+<script lang="ts">
+import type { ICategory } from '@/http'
+import type { PropType } from 'vue'
+
+export default {
+  props: {
+    category: {
+      type: Object as PropType<ICategory>,
+      required: true
+    }
+  }
+}
+</script>
+
+<template>{{ category.name }} {{ category.image }}</template>
