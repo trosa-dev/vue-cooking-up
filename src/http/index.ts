7cbd53c7ead44db3590f6c8dbeeb68c62de2640c
Thiago Rosa <thiagosrosa@gmail.com>
2023-12-24 17:30:20 -0300

    feat: create mock function getCategories

---
+++ src/http/index.ts
@@ -0,0 +1,14 @@
+export function getCategories() {
+	return [
+		{
+			"name": "Dairy and Eggs",
+			"ingredients": ["Eggs", "Cheese", "Milk", "Butter", "Cream", "Yogurt", "Condensed Milk", "Ice Cream"],
+			"label": "dairy_and_eggs"
+		},
+		{
+			"name": "Flours and Leavening Agents",
+			"ingredients": ["Wheat Flour", "Tapioca Flour", "Bread Crumbs", "Hominy Grits", "Cassava Flour", "Cornmeal", "Flaxseed", "Baking Powder"],
+			"label": "flours_and_leavening_agents"
+		}
+	]
+}
