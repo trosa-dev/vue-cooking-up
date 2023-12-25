be702df957284eb1692acf011d81b37181b1eee0
Thiago Rosa <thiagosrosa@gmail.com>
2023-12-25 13:35:27 -0300

    feat: create SelectableIngredient component

---
+++ src/components/SelectableIngredient.vue
@@ -0,0 +1,25 @@
+<script lang="ts">
+import GenericTag from './GenericTag.vue'
+
+export default {
+  components: { GenericTag },
+  props: { ingredient: { type: String, required: true } },
+  data: () => {
+    return {
+      selected: false
+    }
+  }
+}
+</script>
+
+<template>
+  <button class="ingredient" @click="selected = !selected" :aria-pressed="selected">
+    <GenericTag :text="ingredient" :selected="selected" />
+  </button>
+</template>
+
+<style scoped>
+.ingredient {
+  cursor: pointer;
+}
+</style>
