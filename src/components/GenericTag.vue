52ff45f730d2100221a35011caca69af75fd95f7
Thiago Rosa <thiagosrosa@gmail.com>
2023-12-25 13:15:04 -0300

    feat: add "text" and "selected" props to GenericTag component

--- src/components/GenericTag.vue
+++ src/components/GenericTag.vue
@@ -1,24 +1,31 @@
 <script lang="ts">
 export default {
   props: {
-    text: { type: String, required: true }
+    text: { type: String, required: true },
+    selected: { type: Boolean }
   }
 }
 </script>
 
 <template>
-  <span class="ingredient">{{ text }}</span>
+  <span :class="['tag', { selected }]">{{ text }}</span>
 </template>
 
 <style>
-.ingredient {
+.tag {
   display: inline-block;
   border-radius: 0.5rem;
   min-width: 4.25rem;
   padding: 0.5rem;
   text-align: center;
   transition: 0.2s;
-  color: var(--creme, #fffaf3);
+  color: var(--gray);
+  background: var(--light-gray);
+  font-weight: 400;
+}
+
+.tag.selected {
+  color: var(--cream, #fffaf3);
   background: var(--coral, #f0633c);
   font-weight: 700;
 }
