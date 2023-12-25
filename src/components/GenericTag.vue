1fb6fce71d96ae72480f91736b3c2cb8dc69f380
Thiago Rosa <thiagosrosa@gmail.com>
2023-12-25 12:52:22 -0300

    feat: create GenericTag component

---
+++ src/components/GenericTag.vue
@@ -0,0 +1,25 @@
+<script lang="ts">
+export default {
+  props: {
+    text: { type: String, required: true }
+  }
+}
+</script>
+
+<template>
+  <span class="ingredient">{{ text }}</span>
+</template>
+
+<style>
+.ingredient {
+  display: inline-block;
+  border-radius: 0.5rem;
+  min-width: 4.25rem;
+  padding: 0.5rem;
+  text-align: center;
+  transition: 0.2s;
+  color: var(--creme, #fffaf3);
+  background: var(--coral, #f0633c);
+  font-weight: 700;
+}
+</style>
